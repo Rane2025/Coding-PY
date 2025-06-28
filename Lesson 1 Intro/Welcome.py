60d6cc70fa5a761a81f68d5eef78fe52f6be6be0
Rane2025 <avaneeshmrane@gmail.com>
2025-06-28 09:19:04 +0200

    intro

---
+++ Lesson 1 Intro/Welcome.py
@@ -0,0 +1,11 @@
+# print("Hi")
+# print("Welcome to the world of PYTHON!\n  Cool know?\n Continue to code the world of python\n You have",6)
+# print("lessons")
+
+# Variables
+name = input("What is your name ")
+age = input("Enter your age ")
+print("My name is ",name," and my age is ",age )
+
+import keyword
+print("The keywords are\n ", keyword.kwlist )
