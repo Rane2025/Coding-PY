b70c50829ff99376a6300dc43783883c6c8fdb47
Rane2025 <avaneeshmrane@gmail.com>
2025-08-08 18:16:15 +0200

    Add Rock, Paper, Scissors game implementation

---
+++ Lesson 18/HW.py
@@ -0,0 +1,18 @@
+import random
+
+options = ["Rock", "Paper", "Scissors"]
+user_choice = input("Choose Rock, Paper, or Scissors 👾: ")
+computer_choice = random.choice(options)
+print("You chose:", user_choice)
+print("Computer chose:", computer_choice)
+
+if user_choice == computer_choice:
+    print("It's a tie😅!")
+elif user_choice == "Rock" and computer_choice == "Scissors":
+    print("Rock smashes scissors!🪨⚔️ You win!")
+elif user_choice == "Paper" and computer_choice == "Rock":
+    print("Paper covers rock!📄🪨 You win!")
+elif user_choice == "Scissors" and computer_choice == "Paper":
+    print("Scissors cuts paper!✂️📄 You win!")
+else:
+    print("You lose!👾")
