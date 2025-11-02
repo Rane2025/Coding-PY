31443c17eddbb6becad228e8e8b8575f6a45cd6b
Rane2025 <avaneeshmrane@gmail.com>
2025-11-02 13:42:49 +0200

    tkinter

---
+++ School Work/School study.py
@@ -0,0 +1,18 @@
+# Challanging codes 1
+x = 5
+y = 10
+if x > 3 and y < 15:
+    print("Both True")
+else:
+    print("At least one false")
+
+# Challanging codes 2
+score = 95
+bonus = 5
+total = score + bonus
+if total >= 100:
+    print("Perfect")
+else:
+    print("God Job")
+
+# Challanging codes 3
